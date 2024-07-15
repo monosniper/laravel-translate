843512844f713f2a4b887ecabe1aaeebb583225f
Radzi7 <vanteateam@gmail.com>
2024-07-15 10:56:34 +0500

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>Kirano Translate | {{ config('app.name') }}</title>
     @livewireStyles
     <link rel="stylesheet" href="{{ asset('vendor/laravel-translate/css/translate.css') }}">
-    @include('inc.sprite')
+    @include('vendor.laravel-translate.inc.sprite')
 </head>
 <body>
     {{ $slot }}
