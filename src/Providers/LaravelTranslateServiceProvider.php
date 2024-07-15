202e2b42804c50eecd572c3d24c5806eb6337a35
Radzi7 <vanteateam@gmail.com>
2024-07-15 10:10:56 +0500

    upd

--- src/Providers/LaravelTranslateServiceProvider.php
+++ src/Providers/LaravelTranslateServiceProvider.php
@@ -11,7 +11,11 @@ class LaravelTranslateServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../../config/kirano_translate.php' => config_path('kirano_translate.php'),
-            ]);
+            ], 'config');
+
+            $this->publishes([
+                __DIR__.'/../dist' => public_path('vendor/laravel-translate'),
+            ], 'public');
         }
 
         $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
